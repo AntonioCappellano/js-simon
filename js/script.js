04d8582be326968d28f1fbedc405e5ae1eb4d4b3
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-14 18:33:21 +0100

    add eventlistener and check the numbers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const randomNumber = document.getElementById("numbers-list");
 const countdownTimer = document.getElementById("countdown");
 const answersInputForm = document.getElementById("answers-form");
+const finalMessage = document.getElementById("message");
 
-let allNumbers = "";
+// genero 5 numeri casuali
+
+let allNumbers = [];
 for (let i = 0; i < 5; i++) {
   const numbers = Math.floor(Math.random() * 50) + 1;
   console.log(numbers);
-  allNumbers += numbers + " ";
+  allNumbers.push(numbers);
 }
 
-randomNumber.innerHTML = allNumbers;
+randomNumber.innerHTML = allNumbers.join(" ");
+
+// parte un timer di 5 secondi, fine timer compare il form
 
 let countDownSeconds = 5;
 countdownTimer.innerHTML = countDownSeconds;
@@ -23,3 +28,24 @@ const setCountDown = setInterval(function () {
     answersInputForm.classList = "inline";
   }
 }, 1000);
+
+// inserisco i 5 numeri e comparo quanti ne sono stati individuati
+
+answersInputForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userInputs = document.querySelectorAll(".form-control");
+
+  let correctCount = 0;
+  let correctNumbers = [];
+
+  for (let i = 0; i < userInputs.length; i++) {
+    const userValue = parseInt(userInputs[i].value);
+    if (allNumbers.includes(userValue)) {
+      correctCount++;
+      correctNumbers.push(userValue);
+    }
+  }
+
+  finalMessage.innerHTML = `Hai indovinato ${correctCount} numero/i corretto/i
+  `;
+});
