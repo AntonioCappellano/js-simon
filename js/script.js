cefdd3f6d605539382b7397d3bbc7ffe10fbc078
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-14 16:05:54 +0100

    see the 5 random numbers in html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const randomNumber = document.getElementById("numbers-list");
 
+let allNumbers = "";
 for (let i = 0; i < 5; i++) {
   const numbers = Math.floor(Math.random() * 50) + 1;
   console.log(numbers);
+  allNumbers += numbers + " ";
 }
+
+randomNumber.innerHTML = allNumbers;
